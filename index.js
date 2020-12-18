5095733951ca8d88240b613bdb60fd0bd36f3fdd
silveiradba <silveiradba@gmail.com>
2020-12-18 14:00:24 -0300

    novo login

--- index.js
+++ index.js
@@ -3,52 +3,52 @@ function logar(){
     var txtSenha = document.getElementById("txtSenha").value;
     console.log("Valores digitados = " + txtLogin + "/ " + txtSenha);
 
-    // preciso agora montar essa mensagem para enviar ao Back-END através do método POST e
-    // depois tratar o resultado
+    // enviar mensagem p/ Back-End através do método POST e tratar o resultado
 
-    // passo 1 - montar o corpo da mensagem
-    // passo 2 - montar o cabeçalho como se fosse o POSTMAN
-    // passo 3 - enviar a URL com a mensagem
-    // passo 4 - tratar
+    // montar corpo da mensagem
+    // montar cabeçalho como se fosse o POSTMAN
+    // enviar URL com a mensagem
 
-    // passo 1
+    // passo 1 - corpo da msg
     var msgBody = {
         email : txtLogin,
-        racf  : txtLogin,
+        racf : txtLogin,
         senha : txtSenha
     };
 
     // passo 2 - cabecalho
     var cabecalho = {
-        method  : "POST",
-        body    : JSON.stringify(msgBody),
+        method : "POST",
+        body : JSON.stringify(msgBody),
         headers : {
             "content-type":"application/json"
         }
     };
-     
+
     // passo 3 - entrar em contato com o back-end
+    // fetch("http://localhost:8088/login", cabecalho).then(res => console.log(res));
+
     fetch("http://localhost:8088/login", cabecalho).then(res => tratastatus(res));
-}
 
-// passo 4
-function tratastatus(res){
-    if (res.status == 200){
-        // extraindo o conteúdo do corpo da mensagem
-        res.json().then(user => registrarUser(user));
+    // passo 4
+    function tratastatus(res){
+        if (res.status == 200){
+            res.json().then(user => registrarUser(user));
+        }
+        else if (res.status == 401){
+            document.getElementById("msgErro").innerHTML = "Senha Invalida";
+        }
+        else if (res.status == 404){        
+            document.getElementById("msgErro").innerHTML = "Usuário Não Encontrado";
+        }
+        else{
+            document.getElementById("msgErro").innerHTML = "Erro Desconhecido";
+        }
     }
-    else if (res.status == 401){
-        document.getElementById("msgErro").innerHTML = "Senha Invalida";
-    }
-    else if (res.status == 404){
-        document.getElementById("msgErro").innerHTML = "Usuario Não Encontrado";
-    }
-    else{
-        document.getElementById("msgErro").innerHTML = "Erro Desconhecido";
-    }
-}
 
-function registrarUser(user){
-    localStorage.setItem("dashcardUser", JSON.stringify(user));
-    window.location = "agentes.html";
+    function registrarUser(user){
+        localStorage.setItem("dashcardUser", JSON.stringify(user));
+        window.location="agentes.html";
+    }
+
 }
